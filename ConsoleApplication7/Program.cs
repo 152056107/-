6e6f56afe88d2f80c158e1eeaaa0f2ffbde27ca3
wangJinKing <1721135713@qq.com>
2017-09-26 19:17:06 +0800

    the first commit

---
+++ ConsoleApplication7/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+           
+           Console.WriteLine(cal.Jian("woshidashen", "shi"));
+            Console.WriteLine(cal.Jia("woshi", "dashe"));
+        }
+    }
+    public class Calculator
+    {
+        public string Jia(string a, string b)
+        {
+
+            return a + b;
+        }
+        public string Jian(string a, string b)
+        {
+
+            return a.Replace(b, "");
+        }
+        public double Jia(double a, double b)
+        {
+            return a + b;
+        }
+        public double Jian(double a, double b)
+        {
+            return a - b;
+        }
+        public double Cheng(double a, double b)
+        {
+            return a * b;
+
+        }
+        public double Chu(double a, double b)
+        {
+            double result = new double();
+            try
+            {
+                if (b == 0)
+                    throw new DivideByZeroException();
+                else
+                    result = a / b;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("除数不能为零!");
+            }
+
+
+            return result;
+        }
+    }
+}
